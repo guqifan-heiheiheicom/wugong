2881ab1d4f12a9b55c31ce72560a84ecbca6d272
linghuchong <guqifanhei@126.com>
2019-10-22 01:03:34 +0800

    更新2

--- src/wugong/DuGuJiuJian.java
+++ src/wugong/DuGuJiuJian.java
@@ -4,7 +4,9 @@ public class DuGuJiuJian {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println(1111111);
+System.out.println("1111111");
+System.out.println("2222222");
+System.out.println("22222221");
 	}
 
 }
